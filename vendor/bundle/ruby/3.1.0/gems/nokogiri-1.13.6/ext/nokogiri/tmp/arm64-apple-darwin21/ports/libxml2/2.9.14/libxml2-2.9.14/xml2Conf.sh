02e6ba00f414c7dd63af4f01c3537ad8079b49ff
kyle <xionghao@kyledeMacBook-Air.local>
2022-06-14 20:32:44 +0800

    update

---
+++ vendor/bundle/ruby/3.1.0/gems/nokogiri-1.13.6/ext/nokogiri/tmp/arm64-apple-darwin21/ports/libxml2/2.9.14/libxml2-2.9.14/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/Users/xionghao/Desktop/flotuem/flotuem.github.io/vendor/bundle/ruby/3.1.0/gems/nokogiri-1.13.6/ports/arm64-darwin21/libxml2/2.9.14/lib"
+XML2_LIBS="-lxml2 -L/opt/local/lib -lz -L/opt/local/lib -llzma -lpthread  -liconv  -lm "
+XML2_INCLUDEDIR="-I/Users/xionghao/Desktop/flotuem/flotuem.github.io/vendor/bundle/ruby/3.1.0/gems/nokogiri-1.13.6/ports/arm64-darwin21/libxml2/2.9.14/include/libxml2"
+MODULE_VERSION="xml2-2.9.14"
+
---
+++ vendor/bundle/ruby/3.1.0/gems/nokogiri-1.13.6/ports/arm64-darwin21/libxslt/1.1.35/lib/xsltConf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the xslt library
+#
+XSLT_LIBDIR="-L/Users/xionghao/Desktop/flotuem/flotuem.github.io/vendor/bundle/ruby/3.1.0/gems/nokogiri-1.13.6/ports/arm64-darwin21/libxslt/1.1.35/lib"
+XSLT_LIBS="-lxslt -L/Users/xionghao/Desktop/flotuem/flotuem.github.io/vendor/bundle/ruby/3.1.0/gems/nokogiri-1.13.6/ports/arm64-darwin21/libxml2/2.9.14/lib -lxml2 -L/opt/local/lib -lz -L/opt/local/lib -llzma -lpthread -liconv -lm "
+XSLT_PRIVATE_LIBS=""
+XSLT_INCLUDEDIR="-I/Users/xionghao/Desktop/flotuem/flotuem.github.io/vendor/bundle/ruby/3.1.0/gems/nokogiri-1.13.6/ports/arm64-darwin21/libxslt/1.1.35/include"
+MODULE_VERSION="xslt-1.1.35"
